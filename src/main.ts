4e3629f6e0707cb9ea10f33b3cdca8d47fa6013d
sunn4room <sunn4room@163.com>
2022-09-17 21:10:13 +0800

    -

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import * as core from '@actions/core'
 async function run(): Promise<void> {
   try {
     const d = core.getInput('dict')
+    core.info('---')
     core.info(d)
+    core.info('---')
+    const dd = JSON.parse(d)
+    core.info(dd.name)
+    core.info(dd.age.toString())
+    core.info('---')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
