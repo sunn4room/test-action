5bd97c1d8dc3aeae74d6fff13452058e07e07b40
sunn4room <sunn4room@163.com>
2022-09-17 21:01:53 +0800

    -

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,9 @@
 import * as core from '@actions/core'
-import * as http from '@actions/http-client'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    const client = new http.HttpClient('client')
-    const res = await client.get('https://www.baidu.com')
-    if (res.message.statusCode) core.info(res.message.statusCode.toString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const d = core.getInput('dict')
+    core.info(d)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
