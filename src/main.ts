f00f2bc89d0510176b6de48d3149a87126b90d24
sunn4room <sunn4room@163.com>
2022-09-18 08:16:29 +0800

    -

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import core from '@actions/core'
-import exec from '@actions/exec'
+// import exec from '@actions/exec'
 
 async function run(): Promise<void> {
   try {
@@ -11,9 +11,9 @@ async function run(): Promise<void> {
     core.info(dd.name)
     core.info(dd.age.toString())
     core.info('---')
-    const out = await exec.getExecOutput('ls')
-    core.info(out.stdout)
-    core.info('---')
+    // const out = await exec.getExecOutput('ls')
+    // core.info(out.stdout)
+    // core.info('---')
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message)
